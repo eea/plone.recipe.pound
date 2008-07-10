b1f9d5f9f1a527f765e72671745b07e85d6853d6
yboussard <yboussard>
2008-07-10 10:02:38 +0000

    preparing release 0.5.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ This module contains the tool of plone.recipe.pound
 import os
 from setuptools import setup, find_packages
 
-version = '0.5.0'
+version = '0.5.1'
 
 README = os.path.join(os.path.dirname(__file__),
               'plone',
