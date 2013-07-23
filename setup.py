2a545c2d83a0ee73cd80bc0eb1cb1b1854a77e74
Tiberiu Ichim <tiberiu.ichim@eaudeweb.ro>
2013-07-23 10:13:41 +0200

    Prepare for egg release ver 0.5.6-eea

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ This module contains the tool of plone.recipe.pound
 import os
 from setuptools import setup, find_packages
 
-version = '0.5.5'
+version = '0.5.6-eea'
 
 README = os.path.join(os.path.dirname(__file__),
               'plone',
